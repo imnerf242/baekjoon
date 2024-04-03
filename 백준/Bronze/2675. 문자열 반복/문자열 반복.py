d46fee3ae22c96405a13d189b3af8487cf81847d
imnerf242 <164317997+imnerf242@users.noreply.github.com>
2024-04-03 16:41:07 +0900

    Update 문자열 반복.py

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,7 +1,23 @@
 k = int(input())
+la= [] 
+lb = [] 
 
-for x in range(k):
-    a,b = input().split()
-    for y in b:
-        print(y*int(a),end="")
-    print()
+for x in range(0,k):
+    a,b = input().split(" ")
+    la.append(a)
+    lb.append(b)
+
+for x in range(0,k):
+    for y in range(0,len(lb[x])):
+        K = list(lb[x])
+        print(K[y] * int (la[x]),end="")
+    print("")
+    
+#k = int(input())
+#
+#for x in range(0,k):
+#    a,b = input().split(" ")
+#    lb = list(b)
+#    for k in range(0,len(lb)):
+#        print(lb[k] * int(a) ,end="")
+#    print()
