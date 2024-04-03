3f146a135afd1abd63f2b87865fde8801133fd58
imnerf242 <164317997+imnerf242@users.noreply.github.com>
2024-04-03 16:36:37 +0900

    [Bronze II] Title: 문자열 반복, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,14 +1,7 @@
 k = int(input())
-la= [] #['3', '5']
-lb = [] #['abc', '/HTP']
 
-for x in range(0,k):
-    a,b = input().split(" ")
-    la.append(a)
-    lb.append(b)
-
-for x in range(0,k):
-    for y in range(0,len(lb[x])):
-        K = list(lb[x])
-        print(K[y] * int (la[x]),end="")
-    print("")
+for x in range(k):
+    a,b = input().split()
+    for y in b:
+        print(y*int(a),end="")
+    print()
