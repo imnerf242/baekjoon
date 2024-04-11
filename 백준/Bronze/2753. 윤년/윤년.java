46508acbffdf158b1098969d7c12a02219a6cb3e
imnerf242 <164317997+imnerf242@users.noreply.github.com>
2024-04-11 16:38:55 +0900

    [Bronze V] Title: 윤년, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+       String str = br.readLine();
+       StringTokenizer st = new StringTokenizer(str, " ");
+
+       int year = Integer.parseInt(st.nextToken());
+
+       if(((year % 4 == 0) && (year % 100 != 0)) || ((year % 4 == 0) && (year % 400 == 0)) ){
+        System.out.println("1");
+       }
+       else{
+        System.out.println("0");
+       }
+    }
+}
