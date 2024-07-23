7e4f208911008a124dab5498c251319fb5fa6150
imnerf242 <164317997+imnerf242@users.noreply.github.com>
2024-07-23 15:10:36 +0900

    [Bronze I] Title: FizzBuzz, Time: 128 ms, Memory: 14404 KB -BaekjoonHub

---
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int result=0;
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+        String s3 = br.readLine();
+
+        if(s1.matches("[0-9]+")){
+            result = Integer.parseInt(s1)+3;
+        }else{
+            if(s2.matches("[0-9]+")){
+                result = Integer.parseInt(s2)+2;
+            }
+            else{
+                if(s3.matches("[0-9]+")){
+                    result = Integer.parseInt(s3)+1;
+                }
+            }
+        }
+        if(result % 3 == 0 && result % 5 ==0){
+           System.out.println("FizzBuzz");
+        }
+        if(result % 3 == 0 && result % 5 !=0){
+            System.out.println("Fizz");
+        }
+        if(result % 3 != 0 && result % 5 ==0){
+            System.out.println("Buzz");
+        }
+        if(result % 3 != 0 && result % 5 !=0){
+            System.out.println(result);
+        }
+    }
+}
