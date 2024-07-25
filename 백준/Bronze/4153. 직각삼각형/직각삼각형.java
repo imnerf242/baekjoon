208549fc5e779d76c565ea24474827f6d878d1e1
imnerf242 <164317997+imnerf242@users.noreply.github.com>
2024-07-25 16:05:10 +0900

    [Bronze III] Title: 직각삼각형, Time: 124 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+
+        
+
+        while(true){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            
+
+            if(a==0 && b==0 && c==0){
+                break;
+            }
+
+            int[] arr = new int[] {a,b,c};
+            Arrays.sort(arr);
+            
+            if(arr[0]*arr[0] + arr[1]*arr[1] == arr[2]*arr[2]){
+                System.out.println("right");
+                
+            }
+            else{
+                System.out.println("wrong");
+            }
+        }
+    }
+}
