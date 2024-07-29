3ac3515e8a0a88b3a64422c97a257b9bbb0d37d6
imnerf242 <164317997+imnerf242@users.noreply.github.com>
2024-07-29 16:51:24 +0900

    [Bronze II] Title: 벌집, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int count = 1;
+
+        int range = 2;
+
+        if(N==1){
+            System.out.println(1);
+        }
+        else{
+            while(range<=N){
+            
+                range = range + (count * 6);
+                count++;
+            }
+            System.out.println(count);
+        }
+        
+    }
+}
