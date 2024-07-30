9c08152fcd435a4b546b02df5cb79d911bcdf4c7
imnerf242 <164317997+imnerf242@users.noreply.github.com>
2024-07-30 16:47:17 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14176 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,23 +1,22 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        long sum = 0;
-        long hash = 0;
         String str = br.readLine();
-        int pow=1;
 
-        for(int i = 0; i<N; i++){
-            sum += ((int)str.charAt(i) - 96) * pow;
+        long hash = 0;
+        long pow=1;
+
+        for(int i = 0; i < N; i++){
+            hash += ((int)str.charAt(i) - 96) * pow;
             pow = (pow * 31) % 1234567891;
         }
-
-        hash = sum % 1234567891;
-
+        hash = hash % 1234567891;
         System.out.println(hash);
     }
 }
